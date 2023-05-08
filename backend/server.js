0900e6555403e1d77c992ee951e85691ca36142f
Manuel Peñuñuri <107559184+MPenunuri@users.noreply.github.com>
2023-05-08 22:53:10 +0000

    add express listener

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('hola mundo')
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/api/tareas', (req, res) => {
+    res.json({mensaje: 'mostrar las tareas'})
+})
+
+app.listen(port, () => console.log(`Server iniciado en el puerto ${port}`))
