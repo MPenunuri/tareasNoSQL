217b0b188793da4ae0b00231c69cfb087e4ac2f0
Manuel Peñuñuri <107559184+MPenunuri@users.noreply.github.com>
2023-05-08 23:29:18 +0000

    add other routes

--- backend/routes/tareasRoutes.js
+++ backend/routes/tareasRoutes.js
@@ -2,7 +2,20 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.status(200).json({mensaje: 'mostrar las tareas'});
+    res.status(200).json({mensaje: 'Mostrar las tareas'});
 })
+
+router.post('/', (req, res) => {
+    res.status(200).json({mensaje: 'Crear una tarea'});
+})
+
+router.put('/:id', (req, res) => {
+    res.status(200).json({mensaje: `Modificar la tarea ${req.params.id}`});
+})
+
+router.delete('/:id', (req, res) => {
+    res.status(200).json({mensaje: `Eliminar la tarea ${req.params.id}`});
+})
+
 
 module.exports = router;
