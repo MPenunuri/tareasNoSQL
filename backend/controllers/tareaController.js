521410ad638f2815ff2acc8f35212960e09dc4d7
Manuel Peñuñuri <107559184+MPenunuri@users.noreply.github.com>
2023-05-09 17:53:52 +0000

    add controller

---
+++ backend/controllers/tareaController.js
@@ -0,0 +1,23 @@
+const getTareas = (req, res) => {
+    res.status(200).json({mensaje: 'Mostrar las tareas'});
+};
+
+const setTareas = (req, res) => {
+    res.status(200).json({mensaje: 'Crear una tarea'});
+}
+
+const updateTareas = (req, res) => {
+    res.status(200).json({mensaje: `Modificar la tarea ${req.params.id}`});
+}
+
+const deleteTareas = (req, res) => {
+    res.status(200).json({mensaje: `Eliminar la tarea ${req.params.id}`});
+}
+
+
+module.exports = {
+    getTareas,
+    setTareas, 
+    updateTareas, 
+    deleteTareas
+}
--- backend/routes/tareasRoutes.js
+++ backend/routes/tareasRoutes.js
@@ -1,21 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const { getTareas, setTareas, updateTareas, deleteTareas }= require('../controllers/tareaController')
 
-router.get('/', (req, res) => {
-    res.status(200).json({mensaje: 'Mostrar las tareas'});
-})
+router.get('/', getTareas).post('/', setTareas)
 
-router.post('/', (req, res) => {
-    res.status(200).json({mensaje: 'Crear una tarea'});
-})
-
-router.put('/:id', (req, res) => {
-    res.status(200).json({mensaje: `Modificar la tarea ${req.params.id}`});
-})
-
-router.delete('/:id', (req, res) => {
-    res.status(200).json({mensaje: `Eliminar la tarea ${req.params.id}`});
-})
-
+router.put('/:id', updateTareas).delete('/:id', deleteTareas)
 
 module.exports = router;
