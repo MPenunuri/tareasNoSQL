4063a47de90a1f08cb55dcee832099814591c137
Manuel Peñuñuri <107559184+MPenunuri@users.noreply.github.com>
2023-05-10 17:55:33 +0000

    add mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
-const { errorHandler} = require('./middleware/errorMiddleware')
+const { errorHandler} = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000;
+
+connectDB()
 
 const app = express();
 
 app.use('/api/tareas', require('./routes/tareasRoutes'))
 
-app.use(errorHandler)
+//app.use(errorHandler) // aparentemente hay un problema con eso, verificar más adelante
 
 app.listen(port, () => console.log(`Server iniciado en el puerto ${port}`))
