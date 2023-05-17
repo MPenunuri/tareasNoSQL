8681a144bcd4bc8fc9d13cbad5d6a6ff85f032ad
Manuel Peñuñuri <107559184+MPenunuri@users.noreply.github.com>
2023-05-17 23:20:55 +0000

    improve delete tarea function

--- backend/controllers/tareaController.js
+++ backend/controllers/tareaController.js
@@ -1,5 +1,4 @@
 const Tarea = require('../models/tareaModel');
-
 const asyncHandler = require('express-async-handler')
 
 const getTareas = asyncHandler(async (req, res) => {
@@ -24,11 +23,41 @@ const setTareas = asyncHandler(async (req, res) => {
 });
 
 const updateTareas = asyncHandler(async (req, res) => {
-    res.status(200).json({mensaje: `Modificar la tarea ${req.params.id}`});
+
+    const tarea = await Tarea.findById(req.params.id)
+
+    if(!tarea){
+        res.status(400);
+        throw new Error('Tarea no encontrada')
+    }
+
+    if(tarea.user.toString() !== req.user.id){
+        res.status(401);
+        throw new Error('Acceso no autorizado, la tarea no corresponde al usuario.')
+    }
+
+    const tareaModificada = await Tarea.findByIdAndUpdate(req.params.id, req.body, {new:true})
+
+    res.status(200).json(tareaModificada);
 });
 
 const deleteTareas = asyncHandler(async (req, res) => {
-    res.status(200).json({mensaje: `Eliminar la tarea ${req.params.id}`});
+
+    const tarea = await Tarea.findById(req.params.id);
+
+    if(!tarea){
+        res.status(400);
+        throw new Error('Tarea no encontrada')
+    };
+
+    if(tarea.user.toString() !== req.user.id){
+        res.status(401);
+        throw new Error('Acceso no autorizado, la tarea no corresponde al usuario.')
+    }
+
+    await tarea.deleteOne();
+
+    res.status(200).json({ id: req.params.id });
 });
 
 module.exports = {
